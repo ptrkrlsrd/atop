c7035b8c39d32e1451a775b1661adc63ba5f1b29
ptrkrlsrd <ptrkrlsrd@users.noreply.github.com>
2023-09-06 19:38:50 +0200

    Refactored main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,42 @@ use sysinfo::{ProcessExt, System, SystemExt};
 
 const THRESHOLD: f32 = 0.1;
 
+#[derive(Debug)]
 struct ProcessInfo {
     cpu_usage_sum: f32,
     cpu_usage: f32,
     memory_usage_sum: u64,
     memory_usage: u64,
     count: u64,
+}
+
+fn update_process_info(sys: &System, process_info_map: &mut HashMap<String, ProcessInfo>) {
+    for (_pid, proc) in sys.processes() {
+        let entry = process_info_map
+            .entry(proc.name().to_string())
+            .or_insert_with(|| ProcessInfo {
+                cpu_usage_sum: 0.0,
+                memory_usage_sum: 0,
+                count: 0,
+                cpu_usage: 0.0,
+                memory_usage: 0,
+            });
+        entry.cpu_usage_sum += proc.cpu_usage();
+        entry.cpu_usage = proc.cpu_usage();
+        entry.memory_usage_sum += proc.memory();
+        entry.memory_usage = proc.memory();
+        entry.count += 1;
+    }
+}
+
+fn sort_by_cpu_usage(process_vec: &mut Vec<(&String, &ProcessInfo)>) {
+    process_vec.sort_by(|a, b| {
+        let avg_cpu_usage_a = a.1.cpu_usage_sum / a.1.count as f32;
+        let avg_cpu_usage_b = b.1.cpu_usage_sum / b.1.count as f32;
+        avg_cpu_usage_b
+            .partial_cmp(&avg_cpu_usage_a)
+            .unwrap_or(std::cmp::Ordering::Equal)
+    });
 }
 
 fn main() {
@@ -20,38 +50,18 @@ fn main() {
 
     loop {
         print!("\x1B[2J\x1B[1;1H");
+
+        update_process_info(&sys, &mut process_info_map);
+
+        let mut process_vec: Vec<(&String, &ProcessInfo)> = process_info_map.iter().collect();
+        sort_by_cpu_usage(&mut process_vec);
+
         sys.refresh_all();
-        for (_pid, proc) in sys.processes() {
-            let entry = process_info_map
-                .entry(proc.name().to_string())
-                .or_insert(ProcessInfo {
-                    cpu_usage_sum: 0.0,
-                    memory_usage_sum: 0,
-                    count: 0,
-                    cpu_usage: 0.0,
-                    memory_usage: 0,
-                });
-            entry.cpu_usage_sum += proc.cpu_usage();
-            entry.cpu_usage = proc.cpu_usage();
-            entry.memory_usage_sum += proc.memory();
-            entry.memory_usage = proc.memory();
-            entry.count += 1;
-        }
 
         println!(
             "{:<45} | {:<11} | {:<10} | {:<10} | {:<20}",
             "PID", "Avg CPU", "CPU", "Avg RAM", "RAM"
         );
-        let mut process_vec: Vec<(&String, &ProcessInfo)> = process_info_map.iter().collect();
-
-        // Sort the Vec by average CPU usage
-        process_vec.sort_by(|a, b| {
-            let avg_cpu_usage_a = a.1.cpu_usage_sum / a.1.count as f32;
-            let avg_cpu_usage_b = b.1.cpu_usage_sum / b.1.count as f32;
-            avg_cpu_usage_b
-                .partial_cmp(&avg_cpu_usage_a)
-                .unwrap_or(std::cmp::Ordering::Equal)
-        });
 
         for (pid, info) in &process_vec {
             let cpu = info.cpu_usage;
